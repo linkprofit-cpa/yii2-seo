9d71f2eac161aedf78b846331633df903c79dd26
phrlog <kh@linkprofit.com>
2017-09-26 18:28:00 +0300

    bug fix

--- module/controllers/MetaController.php
+++ module/controllers/MetaController.php
@@ -57,7 +57,7 @@ class MetaController extends Controller
     public function actionIndex()
     {
         $dataProvider = new ActiveDataProvider([
-            'query' => SeoPage::find()->where(['OR', ['action_params' => '[]'], ['LIKE', 'action_params', 'slug']])->andWhere(['NOT LIKE', 'view', 'news'])->andWhere(['NOT LIKE', 'view', 'blog'])->andWhere(['NOT LIKE', 'view', 'ajax'])->andWhere(['NOT LIKE', 'view', '/offers/view/'])->with(array_keys(SeoMeta::nameList()))->orderBy([new \yii\db\Expression('CASE WHEN `view` NOT LIKE \'%en%\' THEN 1 ELSE 2 END')]),
+            'query' => SeoPage::find()->where(['OR', ['action_params' => '[]'], ['LIKE', 'action_params', 'slug']])->andWhere(['NOT LIKE', 'view', 'news'])->andWhere(['NOT LIKE', 'view', 'blog'])->andWhere(['NOT LIKE', 'view', 'ajax'])->andWhere(['NOT LIKE', 'view', 'offers'])->with(array_keys(SeoMeta::nameList()))->orderBy([new \yii\db\Expression('CASE WHEN `view` NOT LIKE \'%en%\' THEN 1 ELSE 2 END')]),
             'pagination' => [
                 'pageSize' => 100
             ],
